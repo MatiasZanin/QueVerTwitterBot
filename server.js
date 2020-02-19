1832fd9c88788741f399b10a134707d447107463
Matias Zanin <45440549+MatiasZanin@users.noreply.github.com>
2020-02-19 00:04:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,21 @@ var cliente = new Twiter({
 var generos;
 var endOfLine = require('os').EOL;
 
-// init();
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 console.log("---------------- BOT INICIADO. ----------------");
+
 setInterval(Loop, 1800000);
 
 async function Loop() {
